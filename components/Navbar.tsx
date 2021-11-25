061dca443a6f5c3cb61920bcd23d2641625809e4
Gabriel <gabrieljim641@gmail.com>
2021-11-25 11:18:55 -0400

    Improves signature UX

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import ConnectedAddressContext from "context/ConnectedAddressContext";
-import { Flex, Box, Button, Text } from "@chakra-ui/react";
+import { Flex, Box, Button, Text, useToast } from "@chakra-ui/react";
 import { useEthers } from "@usedapp/core";
 import { trimAccount } from "../utils/helpers";
 import { useEffect } from "react";
@@ -12,6 +12,7 @@ const Navbar = () => {
   );
   const { library, active, account, activateBrowserWallet } = useEthers();
   const [connectedWallet, setConnectedWallet] = useState(account);
+  const toast = useToast();
 
   const handleWalletConnect = async () => {
     activateBrowserWallet();
@@ -31,12 +32,18 @@ const Navbar = () => {
 
   const handleSessionOnAccountChange = async () => {
     if (connectedWallet && account !== connectedWallet) {
-      console.log("destroying session");
       await fetch("/api/destroySession");
       setConnectedWallet("");
       setSessionAddress("");
 
       if (active) {
+        toast({
+          title: "Make sure to sign the message to log in!",
+          status: "info",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
         signInWallet();
       }
     } else {
@@ -64,6 +71,7 @@ const Navbar = () => {
       {account ? (
         <Text fontSize="lg" fontWeight="bold">
           {trimAccount(account)}
+          {!sessionAddress && " (Pending signature)"}
         </Text>
       ) : (
         <Button onClick={handleWalletConnect}>Connect Wallet</Button>
