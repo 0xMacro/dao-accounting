0733c47470b393cbdbe124bc188b9357c430810e
Gabriel <gabrieljim641@gmail.com>
2021-11-29 17:01:00 -0400

    Charting styling

--- components/Dashboard/TransactionsByAccount/TransactionsChartView.tsx
+++ components/Dashboard/TransactionsByAccount/TransactionsChartView.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
 import { theme } from "@chakra-ui/theme";
 import { Transaction } from "types";
 import {
@@ -7,6 +8,7 @@ import {
   XAxis,
   YAxis,
   CartesianGrid,
+  ResponsiveContainer,
   Tooltip,
   Legend,
   ReferenceLine,
@@ -18,25 +20,32 @@ type ChartProps = {
 
 const TransactionsChartView = ({ monthlyTotal }: ChartProps) => {
   return (
-    <BarChart
-      width={1000}
-      height={500}
-      data={monthlyTotal}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <ReferenceLine y={0} stroke="#000" />
-      <Bar dataKey="in" fill={theme.colors.green[500]} />
-      <Bar dataKey="out" fill={theme.colors.red[500]} />
-    </BarChart>
+    <Box width={1000} maxW="100%" overflow="auto">
+      <ResponsiveContainer width="100%" height={500}>
+        <BarChart
+          data={monthlyTotal}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <XAxis dataKey="name" tickMargin={10} style={{ fill: "white" }} />
+          <YAxis type="number" tickMargin={10} style={{ fill: "white" }} />
+          <Tooltip
+            contentStyle={{ background: "black", fontWeight: "bold" }}
+            formatter={(value: any) =>
+              value.toString().replace("-", "") + " ETH"
+            }
+          />
+          <Legend wrapperStyle={{ paddingTop: 20 }} />
+          <ReferenceLine y={0} stroke="#777" />
+          <Bar radius={6} dataKey="in" fill={theme.colors.green[500]} />
+          <Bar radius={6} dataKey="out" fill={theme.colors.red[500]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </Box>
   );
 };
 
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -7,8 +7,8 @@ export const monthlyTotalToNumber = (monthlyTotal: []) => {
   return monthlyTotal.map((month: any) => {
     return {
       ...month,
-      in: ethers.utils.formatEther(month.in).slice(0, 7),
-      out: "-" + ethers.utils.formatEther(month.out).slice(0, 7),
+      in: parseFloat(ethers.utils.formatEther(month.in).slice(0, 7)),
+      out: parseFloat("-" + ethers.utils.formatEther(month.out).slice(0, 7)),
     };
-  });
+  }).reverse();
 };
